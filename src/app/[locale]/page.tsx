43a87aec80e8f5418dd22179d5db62917ffc05b0
snhrayk <ayaka21sunahara@gmail.com>
2025-06-08 20:10:01 +0900

    BlogsとContactの見た目だけ完成

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,4 +1,5 @@
 import Blogs from '@/components/Blogs'
+import Contact from '@/components/Contact'
 import Header from '@/components/Header'
 
 export default function HomePage() {
@@ -6,6 +7,7 @@ export default function HomePage() {
         <div className='w-full'>
             <Header />
             <Blogs />
+            <Contact />
         </div>
     )
 }
---
+++ src/components/Contact.tsx
@@ -0,0 +1,74 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
+export default function Contact() {
+    return (
+        <div className='bg-[#E1D9D6] py-12'>
+            <div className='mx-auto max-w-[1120px]'>
+                <p className='mb-6 text-center text-2xl'>Contact</p>
+                <h2 className='mb-4 text-center text-5xl font-semibold'>WAKARAN ENGLISHのSNSをぜひフォローしてね!</h2>
+                <p className='mb-8 text-center text-lg'>
+                    Lorem Ipsumは、印刷および型版画業界のダミーテキストです。 Lorem Ipsumは、
+                    1500年代以来、業界の標準的なダミーテキストです。
+                </p>
+                <div className='flex gap-6'>
+                    <Image
+                        src='/images/wakaranenglogo1.png'
+                        alt='メンバー全員とALTとの写真'
+                        width={500}
+                        height={400}
+                        className='w-1/2 rounded-3xl border-2 border-gray-800 object-cover'
+                    />
+                    <div className='flex w-1/2 flex-col gap-6'>
+                        <div className='flex gap-6'>
+                            <Image
+                                src='/images/wakaranenglogo1.png'
+                                alt='InstagramのQRコード'
+                                width={170}
+                                height={170}
+                                className='aspect-square rounded-2xl border-2 border-gray-800 object-cover'
+                            />
+                            <div className='flex flex-col justify-end'>
+                                <Link href='#'>
+                                    <Image
+                                        src={'/images/instagram_logo.svg'}
+                                        alt='Instagramのlogo'
+                                        width={40}
+                                        height={40}
+                                        className='h-10 w-10 object-cover'
+                                    />
+                                </Link>
+                                <p className='text-2xl font-medium'>
+                                    Instagramは、メインストリームソーシャルメディアアプリです。
+                                </p>
+                            </div>
+                        </div>
+                        <div className='flex gap-6'>
+                            <Image
+                                src='/images/wakaranenglogo1.png'
+                                alt='YoutubeのQRコード'
+                                width={170}
+                                height={170}
+                                className='aspect-square rounded-2xl border-2 border-gray-800 object-cover'
+                            />
+                            <div className='flex flex-col justify-end'>
+                                <Link href='#'>
+                                    <Image
+                                        src={'/images/youtube_logo.svg'}
+                                        alt='youtubeのlogo'
+                                        width={40}
+                                        height={40}
+                                        className='h-10 w-10 object-cover'
+                                    />
+                                </Link>
+                                <p className='text-2xl font-medium'>
+                                    Youtubeは、メインストリームソーシャルメディアアプリです。
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
