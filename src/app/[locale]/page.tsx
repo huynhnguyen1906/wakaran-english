22134edcf4c6a00bf6a6dadd4643327ab1c51d7d
snhrayk <ayaka21sunahara@gmail.com>
2025-06-08 18:48:43 +0900

    Blogs一旦完了

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,9 +1,11 @@
+import Blogs from '@/components/Blogs'
 import Header from '@/components/Header'
 
 export default function HomePage() {
     return (
         <div className='w-full'>
             <Header />
+            <Blogs />
         </div>
     )
 }
---
+++ src/components/Blogs.tsx
@@ -0,0 +1,22 @@
+import Link from 'next/link'
+
+export default function Blogs() {
+    return (
+        <div className='mx-auto mb-24 max-w-[1120px]'>
+            <h2 className='mb-8 text-center text-5xl font-semibold'>BLOGS</h2>
+            <ul className='mb-8 flex gap-6'>
+                <li className='aspect-[4/3] w-1/3 rounded-2xl bg-gray-300'></li>
+                <li className='aspect-[4/3] w-1/3 rounded-2xl bg-gray-300'></li>
+                <li className='aspect-[4/3] w-1/3 rounded-2xl bg-gray-300'></li>
+            </ul>
+            <div className='text-end'>
+                <Link
+                    href={'#'}
+                    className='bg-[url("/images/more_btn.svg")] bg-contain bg-bottom bg-no-repeat pr-14 text-2xl font-normal'
+                >
+                    More
+                </Link>
+            </div>
+        </div>
+    )
+}
