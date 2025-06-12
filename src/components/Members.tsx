7dde03a8e4183809fba9699dfc6497a0e0b45b3f
snhrayk <ayaka21sunahara@gmail.com>
2025-06-12 12:18:01 +0900

    members修正中

--- src/components/Members.tsx
+++ src/components/Members.tsx
@@ -3,15 +3,13 @@
 import Image from 'next/image'
 
 // 残り行うこと
-// 1.各メンバーの写真を変更する
 // 2.写真にドロップシャドウをつける
 // 3.情報をmessage>jsonデータから取得するように変更する
-// 4.liの文字色を白に変更する
 
 export default function Members() {
     return (
         <div className='relative top-0 mx-auto max-w-[1120px]'>
-            <div className='sticky top-0 z-50 flex flex-col items-center justify-center pt-48 text-center'>
+            <div className='sticky top-0 z-50 flex h-full w-full flex-col items-center justify-center text-center'>
                 <p className='mb-6 text-2xl'>こんにちは</p>
                 <h2 className='mb-4 text-4xl font-semibold'>愉快なメンバーたちの紹介</h2>
                 <p className='mb-[18px] px-24 text-lg'>
@@ -20,123 +18,106 @@ export default function Members() {
                 </p>
             </div>
             {/* aya */}
-            <div className='box-border flex items-center justify-start pt-[30vh]'>
-                <div className='w-[50%-60px]'>
+            <div className='box-border flex w-full items-center justify-start pt-[30vh]'>
+                <div className='w-1/2'>
                     <Image
                         src='/images/aya.webp'
                         alt='ayaの写真'
                         width={500}
                         height={500}
-                        className='aspect-square w-full object-cover object-center'
+                        className='drop-shadow-light aspect-square w-full object-cover object-top'
                     />
                     <div className='flex flex-col gap-2 pt-2 pb-12'>
-                        <h3 className='text-2xl font-normal'>
-                            aya
-                            <span className='ml-2.5 text-base'>2005.02.21</span>
-                        </h3>
-                        <p className='text-base'>洋楽大好きでーす！ 海外にも行ってみたい！</p>
+                        <h3 className='text-main-color text-2xl font-normal'>aya</h3>
+                        <p className='text-main-color text-base'>洋楽大好きでーす！ 海外にも行ってみたい！</p>
                         <ul className='flex gap-2'>
-                            <li className='rounded-[80px] bg-[#43424A] px-5 py-1 text-base text-[#fefefe]'>#兵庫県</li>
-                            <li className='rounded-[80px] bg-[#43424A] px-5 py-1 text-base text-[#fefefe]'>#女性</li>
-                            <li className='rounded-[80px] bg-[#43424A] px-5 py-1 text-base text-[#fefefe]'>#ESFJ-T</li>
+                            <li className='tag-bg-color tag-color rounded-[80px] px-5 py-1 text-base'>#兵庫県</li>
+                            <li className='tag-bg-color tag-color rounded-[80px] px-5 py-1 text-base'>#女性</li>
+                            <li className='tag-bg-color tag-color rounded-[80px] px-5 py-1 text-base'>#ESFJ-T</li>
                         </ul>
                     </div>
                 </div>
             </div>
             {/* ふうたま */}
-            <div className='box-border flex items-center justify-end'>
-                <div className='w-[50%-60px]'>
+            <div className='box-border flex w-full items-center justify-end'>
+                <div className='w-1/2'>
                     <Image
-                        src='/images/aya.webp'
-                        alt='ふうたまの写真(今はayaの写真)'
+                        src='/images/futama.webp'
+                        alt='ふうたまの写真'
                         width={500}
                         height={500}
-                        className='aspect-square w-full object-cover object-center'
+                        className='drop-shadow-light aspect-square w-full object-cover object-center'
                     />
                     <div className='flex flex-col gap-2 pt-2 pb-12'>
-                        <h3 className='text-2xl font-normal'>
-                            ふうたま<span className='ml-2.5 text-base'>2005.09.11</span>
-                        </h3>
-                        <p className='text-base'>海外文化に興味があります!</p>
+                        <h3 className='text-main-color text-2xl font-normal'>ふうたま</h3>
+                        <p className='text-main-color text-base'>海外文化に興味があります!</p>
                         <ul className='flex gap-2'>
-                            <li className='rounded-[80px] bg-[#43424A] px-5 py-1 text-base text-[#fefefe]'>#兵庫県</li>
-                            <li className='rounded-[80px] bg-[#43424A] px-5 py-1 text-base text-[#fefefe]'>#男性</li>
-                            <li className='rounded-[80px] bg-[#43424A] px-5 py-1 text-base text-[#fefefe]'>#ENFP-T</li>
+                            <li className='tag-bg-color tag-color rounded-[80px] px-5 py-1 text-base'>#兵庫県</li>
+                            <li className='tag-bg-color tag-color rounded-[80px] px-5 py-1 text-base'>#男性</li>
+                            <li className='tag-bg-color tag-color rounded-[80px] px-5 py-1 text-base'>#ENFP-T</li>
                         </ul>
                     </div>
                 </div>
             </div>
             {/* いくちゃん */}
-            <div className='box-border flex items-center justify-start'>
-                <div className='w-[50%-60px]'>
+            <div className='box-border flex w-full items-center justify-start'>
+                <div className='w-1/2'>
                     <Image
-                        src='/images/aya.webp'
-                        alt='いくちゃんの写真(今はayaの写真)'
+                        src='/images/ikuchan.webp'
+                        alt='いくちゃんの写真'
                         width={500}
                         height={500}
-                        className='aspect-square w-full object-cover object-center'
+                        className='drop-shadow-light aspect-square w-full object-cover object-center'
                     />
                     <div className='flex flex-col gap-2 pt-2 pb-12'>
-                        <h3 className='text-2xl font-normal'>
-                            いくちゃん<span className='ml-2.5 text-base'>2002.03.11</span>
-                        </h3>
-                        <p className='text-base'>ヨーロッパに行ってみたい!!!</p>
+                        <h3 className='text-main-color text-2xl font-normal'>いくちゃん</h3>
+                        <p className='text-main-color text-base'>ヨーロッパに行ってみたい!!!</p>
                         <ul className='flex gap-2'>
-                            <li className='rounded-[80px] bg-[#43424A] px-5 py-1 text-base text-[#fefefe]'>#沖縄県</li>
-                            <li className='rounded-[80px] bg-[#43424A] px-5 py-1 text-base text-[#fefefe]'>#男性</li>
-                            <li className='rounded-[80px] bg-[#43424A] px-5 py-1 text-base text-[#fefefe]'>#INFP-T</li>
+                            <li className='tag-bg-color tag-color rounded-[80px] px-5 py-1 text-base'>#沖縄県</li>
+                            <li className='tag-bg-color tag-color rounded-[80px] px-5 py-1 text-base'>#男性</li>
+                            <li className='tag-bg-color tag-color rounded-[80px] px-5 py-1 text-base'>#INFP-T</li>
                         </ul>
                     </div>
                 </div>
             </div>
             {/* HuYnH */}
-            <div className='box-border flex items-center justify-end'>
-                <div className='w-[50%-60px]'>
+            <div className='box-border flex w-full items-center justify-end'>
+                <div className='w-1/2'>
                     <Image
-                        src='/images/aya.webp'
-                        alt='HuYnHの写真(今はayaの写真)'
+                        src='/images/huynh.webp'
+                        alt='HuYnHの写真'
                         width={500}
                         height={500}
-                        className='aspect-square w-full object-cover object-center'
+                        className='drop-shadow-light aspect-square w-full object-cover object-center'
                     />
                     <div className='flex flex-col gap-2 pt-2 pb-12'>
-                        <h3 className='text-2xl font-normal'>
-                            HuYnH<span className='ml-2.5 text-base'>2002.06.19</span>
-                        </h3>
-                        <p className='text-base'>日本で生活するのに精一杯です :((((</p>
+                        <h3 className='text-main-color text-2xl font-normal'>HuYnH</h3>
+                        <p className='text-main-color text-base'>日本で生活するのに精一杯です :((((</p>
                         <ul className='flex gap-2'>
-                            <li className='rounded-[80px] bg-[#43424A] px-5 py-1 text-base text-[#fefefe]'>
-                                #ベトナム
-                            </li>
-                            <li className='rounded-[80px] bg-[#43424A] px-5 py-1 text-base text-[#fefefe]'>#男性</li>
-                            <li className='rounded-[80px] bg-[#43424A] px-5 py-1 text-base text-[#fefefe]'>#ENTP-A</li>
+                            <li className='tag-bg-color tag-color rounded-[80px] px-5 py-1 text-base'>#ベトナム</li>
+                            <li className='tag-bg-color tag-color rounded-[80px] px-5 py-1 text-base'>#男性</li>
+                            <li className='tag-bg-color tag-color rounded-[80px] px-5 py-1 text-base'>#ENTP-A</li>
                         </ul>
                     </div>
                 </div>
             </div>
             {/* ユキ a.k.a Alex */}
-            <div className='box-border flex items-center justify-start pb-[80vh]'>
-                <div className='w-[50%-60px]'>
+            <div className='box-border flex w-full items-center justify-start pb-[80vh]'>
+                <div className='w-1/2'>
                     <Image
-                        src='/images/aya.webp'
-                        alt='ユキの写真(今はayaの写真)'
+                        src='/images/yuki.webp'
+                        alt='ユキの写真'
                         width={500}
                         height={500}
-                        className='aspect-square w-full object-cover object-center'
+                        className='drop-shadow-light aspect-square w-full object-cover object-center'
                     />
                     <div className='flex flex-col gap-2 pt-2 pb-12'>
-                        <h3 className='text-2xl font-normal'>
-                            ユキ a.k.a Alex<span className='ml-2.5 text-base'>1999.08.29</span>
-                        </h3>
-                        <p className='text-base'>言語を勉強する時に一番大事なのは「Speaking」です!</p>
+                        <h3 className='text-main-color text-2xl font-normal'>ユキ a.k.a Alex</h3>
+                        <p className='text-main-color text-base'>言語を勉強する時に一番大事なのは「Speaking」です!</p>
                         <ul className='flex gap-2'>
-                            <li className='rounded-[80px] bg-[#43424A] px-5 py-1 text-base text-[#fefefe]'>
-                                #ミャンマー
-                            </li>
-                            <li className='rounded-[80px] bg-[#43424A] px-5 py-1 text-base text-[#fefefe]'>
-                                #Non-Binary
-                            </li>
-                            <li className='rounded-[80px] bg-[#43424A] px-5 py-1 text-base text-[#fefefe]'>#ENFJ</li>
+                            <li className='tag-bg-color tag-color rounded-[80px] px-5 py-1 text-base'>#ミャンマー</li>
+                            <li className='tag-bg-color tag-color rounded-[80px] px-5 py-1 text-base'>#Non-Binary</li>
+                            <li className='tag-bg-color tag-color rounded-[80px] px-5 py-1 text-base'>#ENFJ</li>
                         </ul>
                     </div>
                 </div>
