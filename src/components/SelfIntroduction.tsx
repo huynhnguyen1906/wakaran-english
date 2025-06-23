47bbb6cafffb63372a57314add0c590fae718efb
IkuakiR <linyuzhao3@gmail.com>
2025-06-23 09:09:14 +0900

    enhance responsive and improve mobile layout for Featured Projects and Self Introduction

--- src/components/SelfIntroduction.tsx
+++ src/components/SelfIntroduction.tsx
@@ -10,16 +10,23 @@ export default function SelfIntroduction() {
     const t = useTranslations('selfIntroduction')
 
     return (
-        <div className='mx-auto w-full max-w-[1120px]'>
-            <h2 className='mb-6 text-center text-2xl'>{t('greeting')}</h2>
-            <h3 className='mb-4 text-center text-5xl font-semibold'>
+        <div className='mx-auto w-full max-w-[1120px] max-md:px-6'>
+            <h2 className='mb-6 text-center text-2xl max-md:hidden'>{t('greeting')}</h2>
+            {/* PC用h3 */}
+            <h3 className='mb-4 hidden text-center text-5xl font-semibold md:block'>
                 {t('title')} <span className='main-color'>{t('titleHighlight')}</span>
             </h3>
-            <p className='text-sub-color mx-auto mb-4 w-4/5 text-center text-base leading-8'>
+            {/* スマホ用h3 */}
+            <h3 className='text-left text-[32px] leading-12 font-semibold md:hidden'>
+                {t('title')}
+                <br />
+                <span className='main-color'>{t('titleHighlight')}</span>
+            </h3>
+            <p className='text-sub-color mx-auto mb-4 w-4/5 text-center text-base leading-8 max-md:w-full max-md:text-left'>
                 {renderTextWithLineBreaks(t('description'))}
             </p>
             <div className='w-full'>
-                <div className='relative h-[510px] w-full overflow-hidden rounded-2xl'>
+                <div className='relative h-[510px] w-full overflow-hidden rounded-2xl max-md:h-[194px]'>
                     <Image
                         src='/images/introductionThumb.jpg'
                         alt={t('thumbnailAlt')}
@@ -32,7 +39,7 @@ export default function SelfIntroduction() {
                             alt={t('playButtonAlt')}
                             width={82}
                             height={58}
-                            className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 object-cover object-center'
+                            className='drop-shadow-playBtn absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 object-cover object-center max-md:h-[44px] max-md:w-[62px]'
                         />
                     </Link>
                 </div>
