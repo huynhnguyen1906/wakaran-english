5cf98345b0626b8a5aac333ffd69ec616652e2f6
snhrayk <ayaka21sunahara@gmail.com>
2025-06-24 18:05:16 +0900

    membersの上下の余白とblogのコンポーネント化完了

--- src/components/Members.tsx
+++ src/components/Members.tsx
@@ -8,7 +8,7 @@ export default function Members() {
     const t = useTranslations('members')
 
     return (
-        <div className='mx-auto mt-6 px-6 md:relative md:max-w-[1120px] md:pt-48 md:pb-24'>
+        <div className='mx-auto mt-12 mb-6 px-6 md:relative md:max-w-[1120px] md:pt-48 md:pb-24'>
             <div className='md:sticky md:top-1/2 md:z-20 md:flex md:-translate-y-1/2 md:flex-col md:items-center md:justify-center md:text-center'>
                 <p className='hidden md:mb-6 md:block md:text-2xl'>{t('subtitle')}</p>
                 <h2 className='mb-2 text-[32px] font-semibold md:mb-4 md:text-5xl'>{t('title')}</h2>
