c2c954039355e6db19f23d05d710c8fbfd926e41
snhrayk <ayaka21sunahara@gmail.com>
2025-08-10 18:04:11 +0900

    ブログ詳細ページ作成

---
+++ src/app/[locale]/blogs/[id]/page.tsx
@@ -0,0 +1,13 @@
+import Footer from '@/components/Footer'
+import Header from '@/components/Header'
+import LatestPosts from '@/components/LatestPosts'
+
+export default function BlogDetail() {
+    return (
+        <div>
+            <Header />
+            <LatestPosts />
+            <Footer />
+        </div>
+    )
+}
